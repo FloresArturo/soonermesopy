2e493af8d5f3ae58e6cfaa5ff99a2117648ca422
FloresArturo <92829003+FloresArturo@users.noreply.github.com>
2025-07-08 13:54:30 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="soonermesopy",
+    version="0.1.0",
+    description="Tools to download data from the Oklahoma Mesonet",
+    author="Arturo J. Flores",
+    author_email="artuflo@okstate.edu",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "requests",
+        "tqdm",
+        "openpyxl"
+    ],
+    python_requires=">=3.8",
+)
